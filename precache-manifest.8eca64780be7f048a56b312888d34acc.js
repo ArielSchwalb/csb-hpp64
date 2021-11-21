1ca11b3a767a923ec94a986f34531ad150152ecb
Code Sandbox <hello@codesandbox.io>
2021-11-21 14:55:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8eca64780be7f048a56b312888d34acc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72a9b74fb64770148083",
+    "url": "https://arielschwalb.github.io/csb-hpp64/static/css/main.51da4e1d.chunk.css"
+  },
+  {
+    "revision": "72a9b74fb64770148083",
+    "url": "https://arielschwalb.github.io/csb-hpp64/static/js/main.956dca6c.chunk.js"
+  },
+  {
+    "revision": "8903e06b503638a2e42f",
+    "url": "https://arielschwalb.github.io/csb-hpp64/static/js/runtime~main.e60f54f2.js"
+  },
+  {
+    "revision": "09fc09cca39aeb28cc7e",
+    "url": "https://arielschwalb.github.io/csb-hpp64/static/js/2.28d8c36f.chunk.js"
+  },
+  {
+    "revision": "8dd1753322acbcd27a58204f6e2fb12f",
+    "url": "https://arielschwalb.github.io/csb-hpp64/index.html"
+  }
+];
